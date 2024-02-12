7b9b3d7450e8a5df0e0a2a2b59fc8718a2385fdc
Adelsgit <adelkk.154@gmail.com>
2024-02-12 18:14:27 +0600

    hw2

--- hero.py
+++ hero.py
@@ -18,8 +18,84 @@ class SuperHero:
         return len(self.catchphrase)
 
 
-batman = SuperHero('Брюс Уэйн', 'batman', 'money', 100, 'im batman' )
+batman = SuperHero('Брюс Уэйн', 'batman', 'money', 100, 'im batman')
 
 print(batman)
 print(batman.double_health())
 print(batman.__len__())
+
+
+class EarthHero(SuperHero):
+    earth = 'earth'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def double_health(self):
+        self.fly = True
+        return self.health_points ** 2
+
+    def smile(self):
+        return f'{self.fly} in the {self.fly}_phrase'
+
+
+class SkyHero(SuperHero):
+    sky = 'sky'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        SuperHero.__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def double_health(self):
+        self.fly = True
+        return self.health_points ** 2
+
+    def smile(self):
+        return f'{self.fly} in the {self.fly}_phrase'
+
+
+class SpaceHero(SuperHero):
+    space = 'space'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        SuperHero.__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def double_health(self):
+        self.fly = True
+        return self.health_points ** 2
+
+    def smile(self):
+        return f'{self.fly} in the {self.fly}_phrase'
+
+
+space_hero_example = SpaceHero(
+    name="Galactic Guardian",
+    nickname="Star Voyager",
+    superpower="Cosmic Energy Manipulation",
+    health_points=500,
+    catchphrase="To infinity, and beyond!",
+    damage=75,
+    fly=True  # This can be omitted to use the default value of False
+)
+class Villain(EarthHero):
+    people = 'monster'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=True):
+        super().__init__(name, nickname, superpower, health_points, catchphrase, damage, fly)  # Corrected call
+
+    def gen_x(self):
+        pass
+
+    def crit(self, cls):
+        return self.damage * self.damage
+
+
+villain_example = Villain(name='Jack Napie', nickname='Joker', superpower='smile', health_points=60,
+                          catchphrase='hahaahhahah', damage=15)
+
+villain_example.crit(SkyHero)
